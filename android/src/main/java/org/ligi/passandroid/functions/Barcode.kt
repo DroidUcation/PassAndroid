ab1838cee009a537d80f9f11e42fb6ac0ff09e36
ligi <ligi@ligi.de>
2017-11-12 17:37:46 -0500

    Revert "Improve BarCode generation"
    This reverts commit b9ea91fbb99961d4d3b4eeda9cfc973707d4ac97.

--- android/src/main/java/org/ligi/passandroid/functions/Barcode.kt
+++ android/src/main/java/org/ligi/passandroid/functions/Barcode.kt
@@ -6,7 +6,6 @@ import android.graphics.drawable.BitmapDrawable
 import com.google.zxing.MultiFormatWriter
 import org.ligi.passandroid.model.pass.PassBarCodeFormat
 import org.ligi.tracedroid.logging.Log
-import java.nio.ByteBuffer
 
 
 fun generateBitmapDrawable(resources: Resources, data: String, type: PassBarCodeFormat): BitmapDrawable? {
@@ -34,22 +33,16 @@ fun generateBarCodeBitmap(data: String, type: PassBarCodeFormat): Bitmap? {
 
         // create buffered image to draw to
         // NTFS Bitmap.Config.ALPHA_8 sounds like an awesome idea - been there - done that ..
-        val barcodeImage = Bitmap.createBitmap(width, height, Bitmap.Config.ALPHA_8)
+        val barcode_image = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
 
         // iterate through the matrix and draw the pixels to the image
-        val black = 0xFF.toByte()
-        val white = 0x0.toByte()
-        val buffer = ByteBuffer.allocateDirect(width * height)
-
-        for (y in 0 until height) {
-            for (x in 0 until width) {
-                buffer.put(if (matrix.get(x, if (is1D) 0 else y)) black else white)
+        for (y in 0..height - 1) {
+            for (x in 0..width - 1) {
+                barcode_image.setPixel(x, y, if (matrix.get(x, if (is1D) 0 else y)) 0 else 0xFFFFFF)
             }
         }
 
-        barcodeImage.copyPixelsFromBuffer(buffer.rewind())
-
-        return barcodeImage
+        return barcode_image
     } catch (e: com.google.zxing.WriterException) {
         Log.w("could not write image " + e)
         // TODO check if we should better return some rescue Image here
